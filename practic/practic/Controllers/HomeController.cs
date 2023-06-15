dacb35245f05ec95e325856f1c3ab1220a113eff
Krapivan <dashchinskiy02@mail.ru>
2023-06-15 18:32:35 +0300

    Initial commit

---
+++ practic/practic/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using practic.Models;
+using System.Diagnostics;
+
+namespace practic.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Privacy(string username, string password)
+        {
+            string user = username;
+            string pass = password;
+            ViewBag.password = password; ViewBag.username = username;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
